7fd7ba345479e874dee356a734e30e0374968de5
bplotnick0 <bplotnick0@gmail.com>
2020-10-27 23:38:47 -0400

    finished my stuff

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,6 @@
 package sample;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -212,5 +211,9 @@ public class Controller {
 
             }
         }
+    }
+
+    public void clearText(ActionEvent actionEvent) {
+        textField.clear();
     }
 }
